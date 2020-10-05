ac637b48a50fe60cc9edb2f78d74f5e628e97298
Devin Kendall <devin@atendesigngroup.com>
2020-10-05 15:35:53 -0600

    1.1.5

--- components/deee/deee.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal) {
-  const self = Drupal.behaviors.deeeComponentBehavior = {
-    attach: function (context, settings) {
-      $('.js-deee', context).once('deeeComponentBehavior').each(function () {
-
-      });
-    },
-  }
-})(jQuery, Drupal);
